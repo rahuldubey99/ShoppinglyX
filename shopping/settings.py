40e6fc976b1d52cc89f9ff70a74a877bb4859930
rahuldubey99 <rd995628@gmail.com>
2021-03-07 14:03:05 +0530

    templates

--- shopping/settings.py
+++ shopping/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'shopping.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

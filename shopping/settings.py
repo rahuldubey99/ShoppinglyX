81e72bfb115c87988b64baeac8d1c0941c3edcec
rahuldubey99 <rd995628@gmail.com>
2021-03-08 23:06:09 +0530

    templates

--- shopping/settings.py
+++ shopping/settings.py
@@ -122,10 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATIC_ROOT = BASE_DIR/'static'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR/ 'media'
 LOGIN_REDIRECT_URL ='/profile/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 SESSION_ENGINE = "django.contrib.sessions.backends.db"

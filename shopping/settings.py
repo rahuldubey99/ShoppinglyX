e076c9b3d4b6fccd066a8afcc394481bd9895405
rahuldubey99 <rd995628@gmail.com>
2021-03-15 20:12:46 +0530

    done

--- shopping/settings.py
+++ shopping/settings.py
@@ -21,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECREAT_KEY']
+# SECRET_KEY = os.environ['SECREAT_KEY']
+SECRET_KEY = '*)cn!edp(c=!er(r!$g+9v9i7)p$@7o5$!^vaq#@fbds+@_f64'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["shoppingxly.herokuapp.com",'localhost']
 
